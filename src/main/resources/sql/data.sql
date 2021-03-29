e10516930689106ce444ed5e28c41e77f4672f79
Khanim Huseynzada <xanim.huseynzade@gmail.com>
2021-03-29 09:21:45 +0400

    second commit

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,10 +1,10 @@
 -- Sample categories
 INSERT INTO categories (id, "name") VALUES (1, 'Sci-Fi');
-INSERT INTO categories (id, "name") VALUES (2, 'Economy');
+INSERT INTO categories (id, "name") VALUES (2, 'Art');
 INSERT INTO categories (id, "name") VALUES (3, 'STEM');
 
 
 -- Sample books
-INSERT INTO books (id, name, author, publish_date, category_id) VALUES (1, 'Fahrenheit 451', 'Ray Bradbury', '1953-10-19', 1);
-INSERT INTO books (id, name, author, publish_date, category_id) VALUES (2, 'The Martian', 'Andy Weir', '2011-9-20', 1);
+INSERT INTO books (id, name, author, publish_date, category_id) VALUES (1, 'Steal like an artist', 'Kleon Austin', '2012-02-28', 1);
+INSERT INTO books (id, name, author, publish_date, category_id) VALUES (2, 'TJava How to Program (Java 2)', ' Herb Schildt', '2000-01-03', 1);
 
